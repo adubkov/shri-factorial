a7c62a504dcacbde1bcb3b0c61f19b75e7d9145a
blacked <alexey.dubkov@gmail.com>
2013-08-08 18:52:26 +0400

    Optimized function

--- factorial.js
+++ factorial.js
@@ -2,23 +2,17 @@ function sum_factorial_list(n) {
 
   var f = [ 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 ];
 
-  function split(n) {
-    nArr = [];
+  function isSumFactorial(num) {
+    var isf = 0, n = num;
     while (n) {
-      nArr.push(n % 10);
+      isf += f[(n % 10)];
       n = ~~(n / 10);
     }
-    return nArr;
-  }
-
-  function isSumFactorial(n) {
-    var m = split(n), isf = 0;
-    while (m.length) isf += f[m.pop()];
-    return isf == n;
+    return isf == num;
   }
 
   var sumFact = [];
   while (n--) if (isSumFactorial(n)) sumFact.push(n);
-  return sumFact;
+  return sumFact; 
 
 }
